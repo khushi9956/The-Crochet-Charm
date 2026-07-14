7796289ebf4f9ed9825bbd567408d1b05e8e330e
Khushi Shukla <khushishukl185@gmail.com>
2026-07-14 13:42:22 +0530

    Fix Razorpay integration and checkout

--- the_crochet_charm/settings.py
+++ the_crochet_charm/settings.py
@@ -10,15 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-=
 
-RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
-RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET")
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+RAZORPAY_KEY_ID = os.environ.get("rzp_live_TDIazIPujH0yoT")
+RAZORPAY_KEY_SECRET = os.environ.get("DxRX5Qe4OUWmt5jtMCwQBRyY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
