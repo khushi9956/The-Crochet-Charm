a0983204244980ac2bad39ecabd244f4a986b0a1
Khushi Shukla <khushishukl185@gmail.com>
2026-07-21 00:08:31 +0530

    email

--- the_crochet_charm/settings.py
+++ the_crochet_charm/settings.py
@@ -192,20 +192,14 @@ CSRF_TRUSTED_ORIGINS = [
     "https://the-crochet-charm-next.vercel.app",
        "https://the-crochet-charm-api.onrender.com",
 ]
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
 import os
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
